de9df553a2e3157d0a79d1b0e9cc3d0c9aeb562c
kblessmi2001 <kblessmi2001@gmail.com>
2023-11-04 21:40:05 +0530

    payment page updated

--- Be Charity/Frontend/src/User/Components/Payment.jsx
+++ Be Charity/Frontend/src/User/Components/Payment.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import {
   chakra,
   Box,
@@ -13,232 +13,253 @@ import {
   CloseButton,
   Spacer,
   Input,
-Stack,
-Image,
-InputGroup,
-InputRightElement,
-InputLeftElement
-} from "@chakra-ui/react";
-import { PiEyeBold,PiEyeClosedBold } from "react-icons/pi";
-import { Fade,Slide } from "react-awesome-reveal";
-import { Tabs, TabList, TabPanels, Tab,Heading, TabPanel,TabIndicator,Text, SimpleGrid } from '@chakra-ui/react'
+  Stack,
+  Image,
+  InputGroup,
+  InputRightElement,
+  InputLeftElement,
+} from '@chakra-ui/react';
+import { PiEyeBold, PiEyeClosedBold } from 'react-icons/pi';
+import { Tabs, TabList, TabPanels, Tab, Heading, TabPanel, TabIndicator, Text, SimpleGrid } from '@chakra-ui/react';
 
+import { useNavigate } from 'react-router-dom';
+import { useToast } from '@chakra-ui/react';
 
-import { useNavigate } from "react-router-dom";
-import { useToast } from '@chakra-ui/react'
+const Payment = () => {
+  const [show, setShow] = React.useState(false);
+  const [amount, setAmount] = useState('');
 
-import { useContext } from 'react';
-const Payment= () => {
-  
+  const cardInitialState = {
+    cardNumber: '',
+    cvv: '',
+    expiryDate: '',
+  };
 
-const [show, setShow] = React.useState(false)
-const [amount,setAmount]=useState("");
-let Method={
-  cardNumber:"",
-  cvv:"",
-  expiryDate:"",
-  UPIid:"",
-  UPIpassword:"",
-  NBid:"",
-NBpassword:""
-};
-const [meth,setMeth]=useState(Method);
-const handleClick = () => {setShow(!show)}
-let Navigate=useNavigate()
-const toast = useToast()
+  const upiInitialState = {
+    UPIid: '',
+    UPIpassword: '',
+  };
 
+  const netbankInitialState = {
+    NBid: '',
+    NBpassword: '',
+  };
 
-function HandleSubmit(e){
-e.preventDefault();
-console.log(method);
-// toast({
-//     title: 'Payment Succesfully',
-//     description: "You have brought our package.",
-//     status: 'success',
-//     duration: 3000,
-//     isClosable: true,
-//   })
+  const [cardmeth, setCardmeth] = useState({ ...cardInitialState });
+  const [upimeth, setUpimeth] = useState({ ...upiInitialState });
+  const [netmeth, setNetmeth] = useState({ ...netbankInitialState });
 
+  const handleClick = () => {
+    setShow(!show);
+  };
 
-// Navigate("/")
-}
+  let Navigate = useNavigate();
+  const toast = useToast();
 
+  function HandleSubmit(e) {
+    e.preventDefault();
+    console.log(e);
+    // toast({
+    //     title: 'Payment Successfully',
+    //     description: "You have brought our package.",
+    //     status: 'success',
+    //     duration: 3000,
+    //     isClosable: true,
+    //   })
+
+    // Navigate("/")
+  }
+
+  const handleCreditCard = (e) => {
+    e.preventDefault();
+    console.log('Card Number:', cardmeth.cardNumber);
+    console.log('Expiry Date:', cardmeth.expiryDate);
+    console.log('CVV:', cardmeth.cvv);
+  };
+
+  const handleUpi = (e) => {
+    e.preventDefault();
+    console.log('UPI ID:', upimeth.UPIid);
+    console.log('Password:', upimeth.UPIpassword);
+  };
+
+  const handleNetBanking = (e) => {
+    e.preventDefault();
+    console.log('User Name:', netmeth.NBid);
+    console.log('Password:', netmeth.NBpassword);
+  };
 
   return (
-<>
+    <>
+      <InputGroup m="auto" mt={5} mb={8} w="80%" border="1px solid black" borderRadius="none">
+        <InputLeftElement pointerEvents="none" color="black" fontSize="1.2em" children="$" />
+        <Input
+          type="number"
+          onChange={(e) => setAmount(+e.target.value)}
+          value={amount}
+          borderRadius="none"
+          fontSize="20"
+          textAlign="right"
+          fontWeight="600"
+          required
+          placeholder="Enter Donation Amount"
+        />
+        <InputRightElement pointerEvents="none" color="black" fontSize="18" children=".00" />
+      </InputGroup>
+      <Box fontFamily="poppins" color="black" w="100%" m="auto" mt="10px">
+        <Tabs isFitted variant="unstyled">
+          <TabList mb="1em">
+            <Tab>Card Payment</Tab>
+            <Tab>UPI Payment</Tab>
+            <Tab>Net Banking</Tab>
+          </TabList>
+          <TabIndicator mt="-1.5px" height="2.5px" bg="#79ab2f" borderRadius="1px" />
+          <TabPanels>
+            <TabPanel>
+              <Stack direction="column" w={350} m="auto" textAlign="center" placeItems="center">
+                <form action="" onSubmit={handleCreditCard}>
+                  <Input
+                    size="lg"
+                    type="number"
+                    variant="flushed"
+                    borderBottom="2px solid black"
+                    mt={5}
+                    required
+                    placeholder="Card Number"
+                    value={cardmeth.cardNumber}
+                    onChange={(e) => setCardmeth({ ...cardmeth, cardNumber: e.target.value })}
+                  />
+                  <SimpleGrid columns={2}>
+                    <Input
+                      size="lg"
+                      type="number"
+                      variant="flushed"
+                      borderBottom="2px solid black"
+                      mt={7}
+                      w="95%"
+                      required
+                      placeholder="Expiry Year"
+                      value={cardmeth.expiryDate}
+                      onChange={(e) => setCardmeth({ ...cardmeth, expiryDate: e.target.value })}
+                    />
+                    <Input
+                      size="lg"
+                      type="number"
+                      variant="flushed"
+                      borderBottom="2px solid black"
+                      mb={8}
+                      w="95%"
+                      mt={7}
+                      ml={5}
+                      required
+                      placeholder="CVV"
+                      value={cardmeth.cvv}
+                      onChange={(e) => setCardmeth({ ...cardmeth, cvv: e.target.value })}
+                    />
+                  </SimpleGrid>
+                  <Button
+                    h="50px"
+                    type="submit"
+                    w={{ base: 130, md: 250 }}
+                    mt={5}
+                    letterSpacing="2px"
+                    borderRadius="0"
+                    fontWeight="300"
+                    bg="#79ab2f"
+                    _hover={{ bgColor: "#f7b70d" }}
+                    color="white"
+                    colorScheme="black"
+                  >
+                    SUBMIT
+                  </Button>
+                </form>
+              </Stack>
+            </TabPanel>
+            <TabPanel>
+              <Stack direction="column" m="auto" textAlign="center" placeItems="center">
+                <form action="" onSubmit={handleUpi}>
+                  <Input
+                    mt={5}
+                    variant="flushed"
+                    borderBottom="2px solid black"
+                    size="lg"
+                    type="text"
+                    required
+                    placeholder="UPI ID"
+                    value={upimeth.UPIid}
+                    onChange={(e) => setUpimeth({ ...upimeth, UPIid: e.target.value })}
+                  />
+                  <InputGroup mt={7} variant="flushed" borderBottom="1px solid black" size="lg" type="text" required placeholder="UPI ID">
+                    <Input pr="4.5rem" type={show ? 'text' : 'password'} placeholder="Password" required 
+                    value={upimeth.UPIpassword}
+                    onChange={(e) => setUpimeth({ ...upimeth, UPIpassword: e.target.value })}/>
+                    <InputRightElement width="4.5rem" onClick={handleNetBanking}>
+                      {show ? <PiEyeBold size={20} /> : <PiEyeClosedBold size={20} />}
+                    </InputRightElement>
+                  </InputGroup>
+                  <Button
+                    h="50px"
+                    type="submit"
+                    w={{ base: 130, md: 250 }}
+                    mt={12}
+                    letterSpacing="2px"
+                    borderRadius="0"
+                    fontWeight="300"
+                    bg="#79ab2f"
+                    _hover={{ bgColor: "#f7b70d" }}
+                    color="white"
+                    colorScheme="black"
+                  >
+                    SUBMIT
+                  </Button>
+                </form>
+              </Stack>
+            </TabPanel>
+            <TabPanel>
+              <Stack direction="column" m="auto" textAlign="center" placeItems="center">
+                <form action="" onSubmit={handleNetBanking}>
+                  <Input
+                    size="lg"
+                    type="text"
+                    variant="flushed"
+                    borderBottom="2px solid black"
+                    mt={5}
+                    required
+                    placeholder="User Name"
+                    value={netmeth.NBid}
+                    onChange={(e) => setNetmeth({ ...netmeth, NBid: e.target.value })}
+                  />
+                  <InputGroup size="lg" mt={7} variant="flushed" borderBottom="1px solid black">
+                    <Input pr="4.5rem" type={show ? 'text' : 'password'} placeholder="Password" required 
+                    value={netmeth.NBpassword}
+                    onChange={(e) => setNetmeth({ ...netmeth, NBpassword: e.target.value })}/>
+                    <InputRightElement width="4.5rem" onClick={handleClick}>
+                      {show ? <PiEyeBold size={20} /> : <PiEyeClosedBold size={20} />}
+                    </InputRightElement>
+                  </InputGroup>
+                  <Button
+                    h="50px"
+                    type="submit"
+                    w={{ base: 130, md: 250 }}
+                    mt={12}
+                    letterSpacing="2px"
+                    borderRadius="0"
+                    fontWeight="300"
+                    bg="#79ab2f"
+                    _hover={{ bgColor: "#f7b70d" }}
+                    color="white"
+                    colorScheme="black"
+                  >
+                    SUBMIT
+                  </Button>
+                </form>
+              </Stack>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </>
+  );
+};
 
-<InputGroup m={"auto"} mt={5} mb={8} w={"80%"} border={"1px solid black"} borderRadius={"none"}>
-    <InputLeftElement
-      pointerEvents='none'
-      color='black'
-      fontSize='1.2em'
-      children='$'
-    />
-      <Input  type='number' onChange={(e)=>setAmount(+e.target.value)} value={amount} borderRadius={"none"} fontSize={"20"}  textAlign={"right"} fontWeight={"600"}
-            required  placeholder='Enter Donation Amount' 
-         /> 
-<InputRightElement
-      pointerEvents='none'
-      color='black'
-   fontSize="18"
-      children='.00'
-    />
-  </InputGroup>
-  <Box  fontFamily="poppins"  color={"black"} w={"100%"} m={"auto"} mt={"10px"}>
-<Tabs isFitted variant='unstyled' >
-  <TabList mb='1em'>
-    <Tab>Card Payment</Tab>
-    <Tab>UPI Payment</Tab>
-    <Tab>Net Banking</Tab>
-  </TabList>
-     <TabIndicator
-      mt="-1.5px"
-      height="2.5px"
-      bg="#79ab2f"
-      borderRadius="1px"
-    />
-  <TabPanels>
-    <TabPanel>
-  
-    <Stack direction={"column"} w={350} m={"auto"}  textAlign={"center"} placeItems={"center"}>
-
-<form action="" onSubmit={HandleSubmit}>
-        <Input size='lg' type='number' variant="flushed"
-           borderBottom={"2px solid black"} mt={5}    required  placeholder='Card Number' 
-         />
-   <SimpleGrid columns={2} >
-         <Input size='lg' type='number' variant="flushed"
-           borderBottom={"2px solid black"}  mt={7} w={"95%"}  required  placeholder='Expiry Year' 
-         />   
-   <Input size='lg' type='number'  variant="flushed"
-           borderBottom={"2px solid black"} mb={8} w={"95%"}  mt={7} ml={5}  required  placeholder='CVV' 
-         />    
-      </SimpleGrid>
-      <Button h={"50px"} type='submit' w={{base:130,md:250}}
-          mt={5}
-        
-          letterSpacing={"2px"}
-                borderRadius={"0"}
-                fontWeight={"300"}
-                bg={"#79ab2f"}
-               
-                _hover={{ bgColor: "#f7b70d" }}
-                color={"white"}
-                colorScheme="black"
-              >
-                SUBMIT
-              </Button>
-        </form>
-  </Stack>
- 
-    </TabPanel>
-  
-    <TabPanel>
- 
-    <Stack direction={"column"}  m={"auto"}  textAlign={"center"} placeItems={"center"}>
-
-    <form action=""  onSubmit={HandleSubmit}>
-    <Input mt={5} 
-           variant="flushed"
-           borderBottom={"2px solid black"}
-           size='lg' type='text' 
-        required  placeholder='UPI ID' 
-           
-            
-          />
-
-
-<InputGroup   mt={7}  variant="flushed"
-           borderBottom={"1px solid black"}
-           size='lg' type='text' 
-        required  placeholder='UPI ID' >
-<Input
-pr='4.5rem'
-type={show ? 'text' : 'password'}
-placeholder='Password'
-
-
-required
-/>
-<InputRightElement width='4.5rem' onClick={handleClick}>
-
-  {(show) ?<><PiEyeBold   size={20}  /></> : <>
- <> <PiEyeClosedBold   size={20}  /></>
-  </>}
-
-</InputRightElement>
-</InputGroup>
-<Button h={"50px"} type='submit' w={{base:130,md:250}}
-          mt={12}
-        
-          letterSpacing={"2px"}
-                borderRadius={"0"}
-                fontWeight={"300"}
-                bg={"#79ab2f"}
-               
-                _hover={{ bgColor: "#f7b70d" }}
-                color={"white"}
-                colorScheme="black"
-              >
-                SUBMIT
-              </Button>
-</form>
-</Stack>
-
-</TabPanel>
-<TabPanel>
-
-<Stack direction={"column"}  m={"auto"}  textAlign={"center"} placeItems={"center"}>
-
-<form action="" onSubmit={HandleSubmit}>
-
-        <Input size='lg' type='text' variant="flushed"
-           borderBottom={"2px solid black"} mt={5}    required  placeholder='User Name' 
-         />
-        
-    <InputGroup size='lg'  mt={7}   variant="flushed"
-           borderBottom={"1px solid black"}>
-      <Input
-        pr='4.5rem'
-        type={show ? 'text' : 'password'}
-        placeholder='Password'
-      
-   
-        required
-      />
-      <InputRightElement width='4.5rem' onClick={handleClick}>
-    
-          {(show) ?<><PiEyeBold   size={20}  /></> : <>
-         <> <PiEyeClosedBold   size={20}  /></>
-          </>}
-        
-      </InputRightElement>
-    </InputGroup>
-    <Button h={"50px"} type='submit' w={{base:130,md:250}}
-          mt={12}
-        
-          letterSpacing={"2px"}
-                borderRadius={"0"}
-                fontWeight={"300"}
-                bg={"#79ab2f"}
-               
-                _hover={{ bgColor: "#f7b70d" }}
-                color={"white"}
-                colorScheme="black"
-              >
-                SUBMIT
-              </Button>
-        </form>
-  </Stack>
-
-
-</TabPanel>
-  </TabPanels>
-
-</Tabs>
-</Box>
-</>
-  )
-}
-
-export default Payment
+export default Payment;
